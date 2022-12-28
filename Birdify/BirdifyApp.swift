a7776370e7a2510087cbadb8a3b33ad8e361cf6b
Michael Rudy <michael4rudy@gmail.com>
2022-12-28 15:01:00 -0500

    Initial Commit

---
+++ Birdify/BirdifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  BirdifyApp.swift
+//  Birdify
+//
+//  Created by Michael Rudy on 12/28/22.
+//
+
+import SwiftUI
+
+@main
+struct BirdifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

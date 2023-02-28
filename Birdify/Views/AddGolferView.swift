5fc73b1a6bd167f5e2a7ee5370b2f320d1e45b50
Michael Rudy <michael4rudy@gmail.com>
2023-02-27 21:29:28 -0500

    changes

--- Birdify/Views/AddGolferView.swift
+++ Birdify/Views/AddGolferView.swift
@@ -9,69 +9,72 @@ import SwiftUI
 
 struct AddGolferView: View {
     let golfModel: GolferModel
-    @State private var numberOfGolfers = 1
-    @State private var golfersAdded = 0
-    
-    var body: some View {
-        
-        VStack {
-            Table(golfModel.golfers) {
-                TableColumn("Name", value: \.name)
-                TableColumn("Handicap") {hdc in
-                    Text(String(hdc.handicap))
-                }
-            }
-            Stepper(value: $numberOfGolfers, in: 1...4) {
-                Text("Add \(numberOfGolfers) Golfer(s)")
-                    .font(.title3)
-            }
-            
-            ForEach(0..<numberOfGolfers, id: \.self) { index in
-                GolferInputView(golferAdded: { name, handicap in
-                    golfModel.addGolfer(name: name, handicap: handicap)
-                    golfersAdded += 1
-                })
-            }
-            
-            
-            if golfersAdded == numberOfGolfers {
-                Button("Done") {
-                    // Dismiss the view
-                }
-            }
-        }
-        .padding()
-        
-    }
-}
-
-struct GolferInputView: View {
-    
-    let golferAdded: (String, Int) -> Void
-    
     @State private var name = ""
     @State private var handicap = ""
     
     var body: some View {
+        
         VStack {
-            TextField("Name", text: $name)
-            TextField("Handicap", text: $handicap)
-                .keyboardType(.numberPad)
-            
-            Button("Add") {
-                if let handicapValue = Int(handicap) {
-                    golferAdded(name, handicapValue)
+            List {
+                Section(header: header) {
+                    ForEach(golfModel.golfers) { golfer in
+                        HStack {
+                            Text(golfer.name)
+                            Spacer()
+                            Text(String(golfer.handicap))
+                        }
+                    }
                 }
             }
-            .disabled(name.isEmpty || handicap.isEmpty)
+            .listStyle(InsetGroupedListStyle())
+            VStack(alignment: .center) {
+                TextField("Name", text: $name)
+                    .padding(.bottom, 10)
+                    .multilineTextAlignment(.center)
+                
+                TextField("Handicap", text: $handicap)
+                    .keyboardType(.numberPad)
+                    .padding(.bottom, 10)
+                    .multilineTextAlignment(.center)
+                    .keyboardType(.numberPad)
+                
+                Button("Add") {
+                    if let handicapValue = Int(handicap) {
+                        golfModel.addGolfer(name: name, handicap: handicapValue)
+                        name = ""
+                        handicap = ""
+                    }
+                }
+                .disabled(name.isEmpty || handicap.isEmpty)
+            }
+            .padding()
+            
+//            if golfModel.golfers.isEmpty {
+//                Button("Done") {
+//                    // Dismiss the view
+//                }
+//            }
+    
+            Spacer()
         }
+        .padding()
+    }
+}
 
+var header: some View {
+    HStack {
+        Text("Golfer")
+        Spacer()
+        Text("Handicap")
     }
 }
 
 struct AddGolferView_Previews: PreviewProvider {
     static let golfModel = GolferModel()
     static var previews: some View {
-        AddGolferView(golfModel: golfModel)
+        NavigationStack {
+            AddGolferView(golfModel: golfModel)
+        }
+        
     }
 }
